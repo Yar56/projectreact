eaffb0e9984dbac91ccf3e474abdffd50ca7079e
Yar56 <yarikvavilin@gmail.com>
2020-04-22 23:12:57 +0500

    добавляю class component FindUsers

--- src/components/MainContent/FindUsers/FindUsers.jsx
+++ src/components/MainContent/FindUsers/FindUsers.jsx
@@ -2,47 +2,46 @@ import React from "react";
 import * as axios from "axios";
 import userPng from '../../../assets/image/user.png'
 
-let FindUsers = (props) => {
-	let getUsers = () =>
-	{
-		if (props.users.length === 0) {
+
+
+class FindUsers extends React.Component{
+		constructor(props) {
+			super(props);
+
 			axios.get("https://social-network.samuraijs.com/api/1.0/users")
 				.then(response => {
-					props.setUsers(response.data.items);
+					this.props.setUsers(response.data.items);
 				});
 		}
-	}
 
-
-	return <div>
-		<button onClick={getUsers}>ggggg</button>
-		{
-			props.users.map(u => <div key={u.id}>
-				<div>
-					<div>
-						<img src={u.photos.small != null ? u.photos.small : userPng} alt=""/>
-					</div>
-					<div>
-						{u.followed
-							? <button onClick={ () => {props.unfollow(u.id)} }>ОТПИСАТЬСЯ</button>
-							: <button onClick={ () => {props.follow(u.id)} }>ПОДПИСАТЬСЯ</button>}
-					</div>
-					<div>
-						<div>{u.name}</div>
-						<div>{u.status}</div>
-					</div>
-					<div>
+		render () {
+			return <div>
+				{
+					this.props.users.map(u => <div key={u.id}>
 						<div>
-							{"u.location.city"}
-							{"u.location.country"}
+							<div>
+								<img src={u.photos.small != null ? u.photos.small : userPng} alt=""/>
+							</div>
+							<div>
+								{u.followed
+									? <button onClick={ () => {this.props.unfollow(u.id)} }>ОТПИСАТЬСЯ</button>
+									: <button onClick={ () => {this.props.follow(u.id)} }>ПОДПИСАТЬСЯ</button>}
+							</div>
+							<div>
+								<div>{u.name}</div>
+								<div>{u.status}</div>
+							</div>
+							<div>
+								<div>
+									{"u.location.city"}
+									{"u.location.country"}
+								</div>
+							</div>
 						</div>
-					</div>
-				</div>
-			</div>)
+					</div>)
+				}
+			</div>
 		}
-		</div>
-
-};
-
+}
 
 export default FindUsers;
